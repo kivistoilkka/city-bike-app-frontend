534d43e5e583e82fd3321f719a16da9421736744
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-02-03 17:04:03 +0200

    Basic data fetching from the backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 const App = () => {
+  const [databaseInfo, setDatabaseInfo] = useState({})
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/database_info').then((response) => {
+      setDatabaseInfo(response.data)
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>City Bike App</h1>
+      <p>
+        Database contains {databaseInfo.station_count} stations and{' '}
+        {databaseInfo.journey_count} journeys
+      </p>
     </div>
   )
 }
