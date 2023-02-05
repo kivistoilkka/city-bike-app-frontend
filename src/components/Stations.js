961cd0fddf3e90da1e3c3f08aa656c97094cb322
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-02-05 21:54:35 +0200

    Loading view

--- src/components/Stations.js
+++ src/components/Stations.js
@@ -4,42 +4,51 @@ import { Link } from 'react-router-dom'
 
 const Stations = () => {
   const [stations, setStations] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     axios.get('/api/all_stations_id_decreasing/').then((res) => {
       setStations(res.data)
+      setLoading(false)
     })
   }, [])
 
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Number</th>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Number of departures</th>
-            <th>Number of returns</th>
-          </tr>
-        </thead>
-        <tbody>
-          {stations.map((s) => (
-            <tr key={s.id}>
-              <td>{s.id}</td>
-              <td>
-                {' '}
-                <Link to={`/stations/${s.id}`}>{s.name_fi}</Link>
-              </td>
-              <td>{s.address_fi}</td>
-              <td>{s.departures}</td>
-              <td>{s.returns}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+  const view = () => {
+    if (loading) {
+      return <p>Getting data from database</p>
+    } else {
+      return (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Number</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Number of departures</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stations.map((s) => (
+                <tr key={s.id}>
+                  <td>{s.id}</td>
+                  <td>
+                    {' '}
+                    <Link to={`/stations/${s.id}`}>{s.name_fi}</Link>
+                  </td>
+                  <td>{s.address_fi}</td>
+                  <td>{s.departures}</td>
+                  <td>{s.returns}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+  }
+
+  return <div>{view()}</div>
 }
 
 export default Stations
